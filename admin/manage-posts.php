913bf4b45f39e50d4719eb4585a1af0c1cd2c655
hw <hw1312@qq.com>
2025-04-30 12:57:47 +0800

    文章管理全部同一行显示，前台浏览按钮放到前面

--- admin/manage-posts.php
+++ admin/manage-posts.php
@@ -148,6 +148,11 @@ $isAllPosts = ('on' == $request->get('__typecho_all_posts') || 'on' == \Typecho\
                                                 title="<?php $posts->commentsNum(); ?> <?php _e('评论'); ?>"><?php $posts->commentsNum(); ?></a>
                                         </td>
                                         <td>
+                                            <?php if ('post_draft' != $posts->type): ?>
+                                                <a href="<?php $posts->permalink(); ?>"
+                                                   title="<?php _e('浏览 %s', htmlspecialchars($posts->title)); ?>"><i
+                                                        class="i-exlink"></i></a>
+                                            <?php endif; ?>
                                             <a href="<?php $options->adminUrl('write-post.php?cid=' . $posts->cid); ?>"><?php $posts->title(); ?></a>
                                             <?php
                                             if ('post_draft' == $posts->type) {
@@ -169,11 +174,6 @@ $isAllPosts = ('on' == $request->get('__typecho_all_posts') || 'on' == \Typecho\
                                             <a href="<?php $options->adminUrl('write-post.php?cid=' . $posts->cid); ?>"
                                                title="<?php _e('编辑 %s', htmlspecialchars($posts->title)); ?>"><i
                                                     class="i-edit"></i></a>
-                                            <?php if ('post_draft' != $posts->type): ?>
-                                                <a href="<?php $posts->permalink(); ?>"
-                                                   title="<?php _e('浏览 %s', htmlspecialchars($posts->title)); ?>"><i
-                                                        class="i-exlink"></i></a>
-                                            <?php endif; ?>
                                         </td>
                                         <td class="kit-hidden-mb"><a
                                                 href="<?php $options->adminUrl('manage-posts.php?__typecho_all_posts=off&uid=' . $posts->author->uid); ?>"><?php $posts->author(); ?></a>
